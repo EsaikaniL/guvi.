36e2b54b20c4730552ced91a6d8e8224d4a51b6c
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-18 16:34:01 +0530

    Update and rename natrl.py to suml.py

--- natrl.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input())
-if num1 < 0:
-   print("Enter a positive number")
-else:
-   sum = 0
-   while(num1 > 0):
-       sum += num1
-       num1 -= 1
-   print(sum)
---
+++ suml.py
@@ -0,0 +1,8 @@
+a=int(input())
+for i in range(a):
+   print i
+n=int(input())
+sum=0
+if n==i:
+	sum=sum+i
+	print sum
